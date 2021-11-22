fad67edf845ca9b29dde8253fa235ddb34c45962
ABDERRAHIM AIT TATA <aittataa@gmail.com>
2021-11-22 15:29:34 +0100

    commit

--- lib/app/data/data_sources/remote/rest_api.dart
+++ lib/app/data/data_sources/remote/rest_api.dart
@@ -3,5 +3,4 @@ class RestApi {
   static const String productApi = "/product";
   static const String categoriesApi = "/category";
   static const String brandApi = "/brand";
-  static const String discountsApi = "/discount";
 }
